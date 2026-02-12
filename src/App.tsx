4f01bd2766002fc26f5baad29b76a99814b522c2
ssvgr14-dev <ssvgr14@gmail.com>
2026-02-12 12:46:46 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "@/components/Header";
+import Index from "./pages/Index";
+import GenerateExam from "./pages/GenerateExam";
+import GenerateFromDocument from "./pages/GenerateFromDocument";
+import MyExams from "./pages/MyExams";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/generate" element={<GenerateExam />} />
+          <Route path="/generate-from-doc" element={<GenerateFromDocument />} />
+          <Route path="/my-exams" element={<MyExams />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
